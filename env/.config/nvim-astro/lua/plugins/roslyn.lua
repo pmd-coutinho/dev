8712bbea4c0e71ea907435e7e154de2474964257
Pedro Coutinho <pmd.coutinho@gmail.com>
2024-12-27 19:09:18 +0000

    automated dev commit

---
+++ env/.config/nvim-astro/lua/plugins/roslyn.lua
@@ -0,0 +1,71 @@
+return {
+  -- CSharp support
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "c_sharp" })
+      end
+    end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "csharpier" })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      require("mason").setup {
+        registries = {
+          "github:mason-org/mason-registry",
+          "github:crashdummyy/mason-registry",
+        },
+      }
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "coreclr" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed =
+        require("astrocore").list_insert_unique(opts.ensure_installed, { "roslyn", "csharpier", "netcoredbg" })
+    end,
+  },
+  {
+    "seblj/roslyn.nvim",
+    ft = "cs",
+    opts = {
+      -- your configuration comes here; leave empty for default settings
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = { "Issafalcon/neotest-dotnet", config = function() end },
+    opts = function(_, opts)
+      if not opts.adapters then opts.adapters = {} end
+      table.insert(opts.adapters, require "neotest-dotnet"(require("astrocore").plugin_opts "neotest-dotnet"))
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        cs = { "csharpier" },
+      },
+    },
+  },
+}
